745c13de5147c272fcf5461a90aae21dcc1acde1
Shubhangi <46417551+shubsonawane@users.noreply.github.com>
2020-09-08 16:40:39 +0530

    Update StringReverse.java
    made changes to check update pull in eclipse

--- src/StringReverse.java
+++ src/StringReverse.java
@@ -3,8 +3,8 @@ public class StringReverse {
 	
 	public static void main(String[] args) {
 		
-		String str = "Hello Pune, How are you ?î";
-		//int length = str.length();
+		String str = "Hello Pune, How are you ?‚Äù";
+	
 		//System.out.println(str.charAt(11));
 		
 		String reverse = "";
